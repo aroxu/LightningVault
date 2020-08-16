8a5256c928dfe033980d7ac5e75e619fff2f2b38
B1ackAnge1 <30624945+B1ackAnge1@users.noreply.github.com>
2020-08-16 20:20:57 +0900

    Init

---
+++ .eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+  env: {
+    es6: true,
+    node: true,
+    jest: true
+  },
+  extends: ['plugin:react/recommended', 'standard'],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly'
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: './tsconfig.json',
+    ecmaFeatures: {
+      jsx: true
+    },
+    ecmaVersion: 2018,
+    sourceType: 'module'
+  },
+  plugins: ['react', 'react-hooks', '@typescript-eslint', 'prettier'],
+  rules: { 'prettier/prettier': 'error' },
+  settings: {
+    react: {
+      version: 'detect'
+    }
+  }
+}
